d8e736f557072d33865356f8c560efa0934afb3e
Han Lynn <hlynn93@gmail.com>
2015-06-20 03:30:43 +0800

    Create sign up, sign in, login, register button. Textfields -> First Name, Last, Profile do not get inserted into the database upon sign up. Probably have to use attr_accesible or strong parameter gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :statuses, :dependent => :destroy 
-
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    has_many :statuses, :dependent => :destroy 
+
+		def full_name
+    		first_name + " " + last_name
+		end
 end
