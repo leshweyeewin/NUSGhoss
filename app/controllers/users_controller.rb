ff6a41d06775af649686b66e2b805c22b6277a82
leshweyeewin <www.pancherry.com@gmail.com>
2015-06-28 01:41:43 +0800

    User Profile Page Updated for tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @statuses = Status.where("user_id = ?", current_user.id).order("created_at DESC");
+    @statuses = Status.where("user_id = ?", current_user.id).order("created_at DESC")
+    @tagged_statuses = Status.tagged_with(current_user.profile_name)
   end
 end
