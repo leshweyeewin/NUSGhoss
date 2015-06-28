ac027b1fe4a6add20bde07e06a5d2c0bcfab96a8
leshweyeewin <www.pancherry.com@gmail.com>
2015-06-29 01:49:10 +0800

    Assets Precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,12 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( ivle.js )
 Rails.application.config.assets.precompile += %w( active_admin.js )
 Rails.application.config.assets.precompile += %w( sidebar.js )
-
-Rails.application.config.assets.precompile += %w( bootstrap.css)
-Rails.application.config.assets.precompile += %w( font-awesome.css )
-Rails.application.config.assets.precompile += %w( statuses.css )
-Rails.application.config.assets.precompile += %w( sidebar.css )
-Rails.application.config.assets.precompile += %w( users.css )
+Rails.application.config.assets.precompile += %w( bootstrap.js)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
