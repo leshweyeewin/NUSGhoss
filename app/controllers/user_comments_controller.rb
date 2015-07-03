1c4dcafaa500efd6f2f63f50ea00d5e22b59ebbe
leshweyeewin <www.pancherry.com@gmail.com>
2015-07-04 00:53:07 +0800

    Enabled user comment editing and deleting
    Current edit form is redirected to a new page.
    Consider: To be able to edit comment at the same page or do in-place
    editing.

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -1,5 +1,4 @@
 class UserCommentsController < InheritedResources::Base
-
   def create
   	@user_comment = current_user.user_comments.build(params[:user_comment])
   	if @user_comment.save
@@ -7,6 +6,28 @@ class UserCommentsController < InheritedResources::Base
   	else
   		redirect_to @user_comment.status, :notice => "Error creating comment : #{@user_comment.errors}."	
   	end
+  end
+
+  def edit
+  	@user_comment = UserComment.find(params[:id])
+  	@status = @user_comment.status
+  end
+
+  def update
+  	@user_comment = UserComment.find(params[:id])
+  	@status = @user_comment.status
+	if @user_comment.update(params[:user_comment])
+  		redirect_to @status, :notice => "Comment was successfully updated."
+  	else
+  		redirect_to @status, :notice => "Error creating comment : #{@user_comment.errors}."	
+    end
+  end
+
+  def destroy
+  	@user_comment = UserComment.find(params[:id])
+  	@status = @user_comment.status
+  	@user_comment.destroy
+  	redirect_to @status, :notice => "Comment was successfully destroyed."
   end
 end
 
