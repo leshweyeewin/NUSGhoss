fecfa77c6ee1b3aaf3dcbfa8c6d41a357fa28ce0
leshweyeewin <www.pancherry.com@gmail.com>
2015-08-01 04:03:35 +0800

    non registered user logged in with facebook error resolve

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,13 +5,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
+    if @user.nil?
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to "https://ivle.nus.edu.sg/api/login/?apikey=nt5s4waVtfzugEGRSuW5Z&url=http%3A%2F%2Flocalhost%3A3000%2Fusers%2Fsign_up"
+    else
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      end
     end
   end
 end
