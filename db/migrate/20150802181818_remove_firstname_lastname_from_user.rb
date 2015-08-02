5379dc61b9784c7aed1db99e4fa752c62f605a24
leshweyeewin <www.pancherry.com@gmail.com>
2015-08-03 03:40:56 +0800

    Migration file updated.

--- db/migrate/20150802181818_remove_firstname_lastname_from_user.rb
+++ db/migrate/20150802181818_remove_firstname_lastname_from_user.rb
@@ -2,6 +2,5 @@ class RemoveFirstnameLastnameFromUser < ActiveRecord::Migration
   def change
     remove_column :users, :first_name, :string
     remove_column :users, :last_name, :string
-    remove_column :users, :ivlename, :string
   end
 end
