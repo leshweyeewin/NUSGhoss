d3a95a59343119613b2df7ca08b0d609499a7776
leshweyeewin <www.pancherry.com@gmail.com>
2015-08-05 02:42:31 +0800

    Admin panel updated for new fields in User database

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register User do
   actions :all, :except => [:new, :edit]
-  permit_params :first_name, :last_name, :profile_name, :email, :password, :password_confirmation
+  permit_params :ivle_id, :ivle_name, :profile_name, :email, :password, :password_confirmation
 
   index do
     selectable_column
     id_column
-    column :first_name
-    column :last_name
+    column "User ID", :ivle_id
+    column "User Name", :ivle_name
     column :profile_name
     column :email
     column :current_sign_in_at
@@ -15,8 +15,8 @@ ActiveAdmin.register User do
     actions
   end
   
-  filter :first_name
-  filter :last_name
+  filter :ivle_id
+  filter :ivle_name
   filter :profile_name
   filter :email
   filter :current_sign_in_at
@@ -25,8 +25,6 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs "User Details" do
-      f.input :first_name
-      f.input :last_name
       f.input :profile_name
       f.input :email
       f.input :password
@@ -34,5 +32,4 @@ ActiveAdmin.register User do
     end
     f.actions
   end
-
 end
